ab41dcfd623a85341f04abcd6459ef22176ad240
Nikhil Patel <npatelaz@me.com>
2014-04-05 11:47:38 -0700

    Added Main class and method

---
+++ src/me/npatelaz/functiongrapher/Main.java
@@ -0,0 +1,24 @@
+package me.npatelaz.functiongrapher;
+
+import javax.swing.*;
+
+/**
+ * Description
+ * <p/>
+ * Nikhil Patel
+ * File created on Apr 05, 2014
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Set up frame (main application window)
+        JFrame frame = new JFrame();
+        frame.setTitle("Function Grapher");
+        frame.setSize(850, 600);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
