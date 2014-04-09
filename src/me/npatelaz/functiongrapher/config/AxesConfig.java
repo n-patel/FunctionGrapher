41ad71674143e6945f3df6a41102fb63cd6f2c5e
Nikhil Patel <npatelaz@me.com>
2014-04-09 09:28:30 -0700

    Cleaned up the hideous repetitiveness of AxesConfig

--- src/me/npatelaz/functiongrapher/config/AxesConfig.java
+++ src/me/npatelaz/functiongrapher/config/AxesConfig.java
@@ -18,39 +18,20 @@ public class AxesConfig extends JPanel
 		MigLayout migLayout = new MigLayout("wrap 2");
 		setLayout(migLayout);
 
-		JTextField xmin = new JTextField(7);
-		JTextField xmax = new JTextField(7);
-		JTextField xscl = new JTextField(7);
-		JTextField ymin = new JTextField(7);
-		JTextField ymax = new JTextField(7);
-		JTextField yscl = new JTextField(7);
-
 		AxesConfigListener axesConfigListener = new AxesConfigListener();
-		xmin.addActionListener(axesConfigListener);
-		xmax.addActionListener(axesConfigListener);
-		xscl.addActionListener(axesConfigListener);
-		ymin.addActionListener(axesConfigListener);
-		ymax.addActionListener(axesConfigListener);
-		yscl.addActionListener(axesConfigListener);
 
-		xmin.setActionCommand("XMIN");
-		xmax.setActionCommand("XMAX");
-		xscl.setActionCommand("XSCL");
-		ymin.setActionCommand("YMIN");
-		ymax.setActionCommand("YMAX");
-		yscl.setActionCommand("YSCL");
+		String[] labels = {"XMIN", "XMAX", "XSCL", "YMIN", "YMAX", "YSCL"};
 
-		add(new JLabel("XMIN"));
-		add(xmin, "gapleft 50");
-		add(new JLabel("XMAX"));
-		add(xmax, "gapleft 50");
-		add(new JLabel("XSCL"));
-		add(xscl, "gapleft 50");
-		add(new JLabel("YMIN"));
-		add(ymin, "gapleft 50");
-		add(new JLabel("YMAX"));
-		add(ymax, "gapleft 50");
-		add(new JLabel("YSCL"));
-		add(yscl, "gapleft 50");
+		for (int i = 0; i < labels.length; i++)
+		{
+			JTextField tf = new JTextField(7);
+			tf.addActionListener(axesConfigListener);
+			tf.setActionCommand(labels[i]);
+
+			add(new JLabel(labels[i]));
+			add(tf, "gapleft 50");
+		}
+
 	}
+
 }
