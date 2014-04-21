d8fe41b03d53d3de5075d16551767aad974712f7
Nikhil Patel <npatelaz@me.com>
2014-04-21 10:45:30 -0700

    Moved WolframAlpha query code to separate util class

--- src/me/npatelaz/functiongrapher/config/WolframAlphaDisplay.java
+++ src/me/npatelaz/functiongrapher/config/WolframAlphaDisplay.java
@@ -1,7 +1,5 @@
 package me.npatelaz.functiongrapher.config;
 
-import com.wolfram.alpha.*;
-
 /**
  * Description
  *
@@ -19,62 +17,7 @@ public class WolframAlphaDisplay
 
 	public void initializePanel()
 	{
-		WAEngine engine = new WAEngine();
-		engine.setAppID("KGQA7X-WR86X7AL2K");
-		engine.addFormat("plaintext");
 
-		WAQuery query = engine.createQuery();
-
-		query.setInput(input);
-
-		try
-		{
-			System.out.println("Query URL:");
-			System.out.println(engine.toURL(query));
-			System.out.println("");
-
-			WAQueryResult queryResult = engine.performQuery(query);
-
-			if (queryResult.isError())
-			{
-				System.out.println("Query error");
-				System.out.println("  error code: " + queryResult.getErrorCode());
-				System.out.println("  error message: " + queryResult.getErrorMessage());
-			}
-			else if (!queryResult.isSuccess())
-			{
-				System.out.println("Query was not understood; no results available.");
-			}
-			else
-			{
-				// Got a result.
-				System.out.println("Successful query. Pods follow:\n");
-				for (WAPod pod : queryResult.getPods())
-				{
-					if (!pod.isError())
-					{
-						System.out.println(pod.getTitle());
-						System.out.println("------------");
-						for (WASubpod subpod : pod.getSubpods())
-						{
-							for (Object element : subpod.getContents())
-							{
-								if (element instanceof WAPlainText)
-								{
-									System.out.println(((WAPlainText) element).getText());
-									System.out.println("");
-								}
-							}
-						}
-						System.out.println("");
-					}
-				}
-			}
-		}
-		catch (WAException e)
-		{
-			e.printStackTrace();
-		}
 	}
 
 }
---
+++ src/me/npatelaz/functiongrapher/util/WolframAlphaHelper.java
@@ -0,0 +1,77 @@
+package me.npatelaz.functiongrapher.util;
+
+import com.wolfram.alpha.*;
+
+/**
+ * Description
+ *
+ * Nikhil Patel
+ * File created on Apr 21, 2014
+ */
+public class WolframAlphaHelper
+{
+	/**
+	 * Returns a string containing the query result for a given input
+	 * @param queryText     text to query
+	 * @return              query result from WolframAlpha
+	 */
+	public static String query(String queryText)
+	{
+		// Set up WolframAlpha engine
+		WAEngine engine = new WAEngine();
+		engine.setAppID("KGQA7X-WR86X7AL2K");
+		engine.addFormat("plaintext");
+
+		// Set up WolframAlpha query
+		WAQuery query = engine.createQuery();
+		query.setInput(queryText);
+
+		WAQueryResult queryResult;
+		String queryResultText = "";
+
+		try
+		{
+			queryResult = engine.performQuery(query);
+
+			if (queryResult.isError())
+			{
+				queryResultText = "Query error: " + queryResult.getErrorMessage() + "(code " + queryResult.getErrorCode() + ").";
+			}
+			else if (!queryResult.isSuccess())
+			{
+				queryResultText = "No results available.";
+			}
+			else
+			{
+				for (WAPod pod : queryResult.getPods())
+				{
+					if (!pod.isError())
+					{
+						queryResultText += pod.getTitle() + "\n";
+						queryResultText += "------------" + "\n";
+
+						for (WASubpod subpod : pod.getSubpods())
+						{
+							for (Object element : subpod.getContents())
+							{
+								if (element instanceof WAPlainText)
+								{
+									queryResultText += ((WAPlainText)element).getText() + "\n";
+								}
+							}
+						}
+						queryResultText += "\n";
+					}
+				}
+			}
+		}
+		catch (WAException e)
+		{
+			e.printStackTrace();
+		}
+
+		return queryResultText;
+	}
+
+}
+
