3e2e93920ddab92494657b47ed1a20c0653459c6
Nikhil Patel <npatelaz@me.com>
2014-04-22 11:21:43 -0700

    Exposed instance fields and added getters/setters

--- src/me/npatelaz/functiongrapher/config/AxesConfig.java
+++ src/me/npatelaz/functiongrapher/config/AxesConfig.java
@@ -12,6 +12,13 @@ import javax.swing.*;
  */
 public class AxesConfig extends JPanel
 {
+	private JTextField xmin;
+	private JTextField xmax;
+	private JTextField xscl;
+	private JTextField ymin;
+	private JTextField ymax;
+	private JTextField yscl;
+
 	/**
 	 * Initializes the JPanel with the axes configuration text fields
 	 */
@@ -19,18 +26,86 @@ public class AxesConfig extends JPanel
 	{
 		AxesConfigListener axesConfigListener = new AxesConfigListener();
 
-		String[] labels = {"XMIN", "XMAX", "XSCL", "YMIN", "YMAX", "YSCL"};
+		xmin = new JTextField(10);
+		xmax = new JTextField(10);
+		xscl = new JTextField(10);
+		ymin = new JTextField(10);
+		ymax = new JTextField(10);
+		yscl = new JTextField(10);
 
-        for (String label : labels) {
-            JTextField tf = new JTextField(10);
-	        tf.setHorizontalAlignment(JTextField.CENTER);
-	        tf.addActionListener(axesConfigListener);
-            tf.setActionCommand(label);
+		JTextField[] textFields =   { xmin,   xmax,   xscl,   ymin,   ymax,   yscl };
+		String[] labels =           {"XMIN", "XMAX", "XSCL", "YMIN", "YMAX", "YSCL"};
 
-            add(new JLabel(label));
-            add(tf, "gapleft 55");
+        for (int i = 0; i < textFields.length; i++)
+        {
+	        textFields[i].setHorizontalAlignment(JTextField.CENTER);
+	        textFields[i].addActionListener(axesConfigListener);
+	        textFields[i].setActionCommand(labels[i]);
+
+            add(new JLabel(labels[i]));
+            add(textFields[i], "gapleft 55");
         }
 
+	}
+
+	public String getXmin()
+	{
+		return xmin.getText();
+	}
+
+	public void setXmin(String xmin)
+	{
+		System.out.println(this.xmin);
+		this.xmin.setText(xmin);
+	}
+
+	public String getXmax()
+	{
+		return xmax.getText();
+	}
+
+	public void setXmax(String xmax)
+	{
+		System.out.println(this.xmax);
+		this.xmax.setText(xmax);
+	}
+
+	public String getXscl()
+	{
+		return xscl.getText();
+	}
+
+	public void setXscl(String xscl)
+	{
+		this.xscl.setText(xscl);
+	}
+
+	public String getYmin() {
+		return ymin.getText();
+	}
+
+	public void setYmin(String ymin)
+	{
+		this.ymin.setText(ymin);
+	}
+
+	public String getYmax() {
+		return ymax.getText();
+	}
+
+	public void setYmax(String ymax)
+	{
+		this.ymax.setText(ymax);
+	}
+
+	public String getYscl()
+	{
+		return yscl.getText();
+	}
+
+	public void setYscl(String yscl)
+	{
+		this.yscl.setText(yscl);
 	}
 
 }
